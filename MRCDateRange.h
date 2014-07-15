b75e42fc0534b7d562db74e6356aa3ce0e7cfad4
Mathew Cruz <jonusx@gmail.com>
2014-07-15 15:33:04 -0400

    Added documentation for MRCDateRange

--- MRCDateRange.h
+++ MRCDateRange.h
@@ -24,15 +24,63 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ `MRCDateRange` Is a basic range using two dates
+ */
 @interface MRCDateRange : NSObject
 
 @property (nonatomic, strong, readonly) NSDate *startDate;
 @property (nonatomic, strong, readonly) NSDate *endDate;
 
+/**
+ Designated initializer
+ 
+ @param startDate `NSDate` object to use for the start of the date range.
+ @param endDate `NSDate` object to use for the end of the date range.
+ 
+ @return Initialized `MRCDateRange` object.
+ */
 - (instancetype)initWithStartDate:(NSDate *)startDate endDate:(NSDate *)endDate;
+
+/**
+ Convenience initializer, calls initWithStartDate:endDate:
+ 
+ @param startDate `NSDate` object to use for the start of the date range.
+ @param endDate `NSDate` object to use for the end of the date range.
+ 
+ @return Initialized `MRCDateRange` object.
+ */
 + (instancetype)dateRangeWithStartBeginningDate:(NSDate *)startDate endDate:(NSDate *)endDate;
+
+/**
+ Convenience initializer, builds date range with todays date for given `NSCalendarUnit`
+ Ex: Given `NSCalendarUnitWeekOfMonth`, this will return a date range for the week that today is contained in
+ 
+ @param calendarUnitRange `NSCalendarUnit` to construct around todays date
+ 
+ @return Initialized `MRCDateRange` object.
+ */
 + (instancetype)dateRangeWithTodayForUnitRange:(NSCalendarUnit)calendarUnitRange;
+
+/**
+ Convenience initializer, builds date range with todays date for given `NSCalendarUnit`
+ 
+ @param date `NSDate` to construct date range in
+ @param calendarUnitRange `NSCalendarUnit` to construct with
+ 
+ @return Initialized `MRCDateRange` object.
+ */
 + (instancetype)dateRangeAroundDate:(NSDate *)date forUnitRange:(NSCalendarUnit)calendarUnitRange;
 
+/**
+ Convenience method returning the start date for an `NSCalendarUnit`
+ Ex: Given `NSCalendarUnitWeekOfMonth`, returns the start of the week for the provided date
+ 
+ @param unit `NSCalendarUnit` for the start date
+ @param date `NSDate` to use to find the appropriate start date
+ 
+ @return An `NSDate` object for the starting date for the givin information
+ */
 + (NSDate *)startDateForUnit:(NSCalendarUnit)unit date:(NSDate *)date;
+
 @end
